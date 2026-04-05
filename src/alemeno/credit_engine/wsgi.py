7dd7184503486fa48f0583554ca8ac66518102f5
Pranjalmishr1 <24pranjalmishra2005@gmail.com>
2026-04-06 02:44:14 +0530

    Completed project

--- src/alemeno/credit_engine/wsgi.py
+++ src/alemeno/credit_engine/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for alemeno project.
+WSGI config for credit_engine project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alemeno.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'credit_engine.settings')
 
 application = get_wsgi_application()
